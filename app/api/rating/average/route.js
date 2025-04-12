549e8ed6b210cc3541dd4b989f39b553bf8efc30
Aldi Denaldi <aldidenaldi@gmail.com>
2025-04-12 10:19:18 +0700

    Fix: add mongodb.js and correct path

---
+++ app/api/rating/average/route.js
@@ -0,0 +1,26 @@
+import clientPromise from '../../../lib/mongodb';
+
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    const { rating } = await req.json();
+
+    if (!rating || typeof rating !== "number" || rating < 1 || rating > 5) {
+      return NextResponse.json({ message: "Rating tidak valid" }, { status: 400 });
+    }
+
+    const client = await connectMongo();
+    const db = client.db(); // ambil default DB dari koneksi URI
+
+    await db.collection("ratings").insertOne({
+      rating,
+      createdAt: new Date(),
+    });
+
+    return NextResponse.json({ message: "Rating berhasil disimpan ke MongoDB" }, { status: 200 });
+  } catch (error) {
+    console.error("MongoDB error:", error);
+    return NextResponse.json({ message: "Gagal simpan rating" }, { status: 500 });
+  }
+}
